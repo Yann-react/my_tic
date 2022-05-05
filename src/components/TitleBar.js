f5ea1d5f9be8ed5c180b69904c21ebac69332405
Guymaster <67753626+Guymaster@users.noreply.github.com>
2022-05-05 12:19:42 +0000

    Create TitleBar.js

---
+++ src/components/TitleBar.js
@@ -0,0 +1,12 @@
+import '../../styles/styleTitleBar.css';
+export default function TitleBar({titre, nombre, onFiltreClick}){
+    return (
+        <div id='bar'>
+            <div id='filtreBTN' onClick={onFiltreClick}>
+                <span>Filtres</span>
+            </div>
+            <div id='titre'> {titre} </div>
+            <div id='nombre'> {nombre} </div>
+        </div>
+    );
+}
