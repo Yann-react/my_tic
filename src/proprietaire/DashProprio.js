1f9074c1731017ed9bc3fb8cb1a6916ce4cb52e0
Guymaster <67753626+Guymaster@users.noreply.github.com>
2022-05-05 13:14:51 +0000

    Update DashProprio.js

--- src/proprietaire/DashProprio.js
+++ src/proprietaire/DashProprio.js
@@ -1,1 +1,54 @@
+import '../styles/styleDashProprio.css';
+import Entete from "../components/entete/entete.js";
+import TitleBar from '../components/entete/TitleBar';
+import ProprioItem from '../components/ProprioItem';
 
+export default function DashProprio(){
+    return (
+        <div>
+            <Entete nomComplet="AMANI KONE" lienProfil="#"/>
+            <TitleBar titre='PROPRIETAIRES' nombre={25} onFiltreClick={cacherOuMontrerFiltres}/>
+            <div id='corps'>
+                <div className='fitreForm filtreFormHidden' id='filtreForm'>
+                    <div className='filtreLabel'>Ville Recherchée</div>
+                    <div className='filtreRow'>
+                        <input type='text'/>
+                    </div>
+                    <div className='filtreLabel'>Commune Recherchée</div>
+                    <div className='filtreRow'>
+                        <input type='text'/>
+                    </div>
+                    <div className='filtreLabel'>Quartier Recherché</div>
+                    <div className='filtreRow'>
+                        <input type='text'/>
+                    </div>
+                    <div className='filtreLabel'>Nombre de Conducteurs Recherchés</div>
+                    <div className='filtreRow'>
+                        <input type='number' min="1" step='1'/>
+                    </div>
+                </div>
+            <div id='listeSommaire'>
+                <div>Date</div>
+                <div>Propriétaire</div>
+                <div>Télephone</div>
+                <div>Véhicules Disponibles</div>
+                <div>Adresse Recherchée</div>
+            </div>
+            <div id='listeBox'>
+            </div>
+            </div>
+        </div>
+        
+    );
+}
+
+
+function cacherOuMontrerFiltres(){
+    let filtreBox = document.getElementById('filtreForm');
+    if(filtreBox.getAttribute('class') == 'fitreForm filtreFormHidden'){
+        filtreBox.setAttribute('class', 'fitreForm');
+    }else{
+        filtreBox.setAttribute('class', 'fitreForm filtreFormHidden');
+    }
+
+}
