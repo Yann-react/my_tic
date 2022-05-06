7bd6cb82de0916fc44b18cc136bd3bf90e2f5efa
Yann-react <blamusjunior@gmail.com>
2022-05-06 15:07:02 +0200

    p

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import filter from '../assets/filter.svg'
 import '../styles/styleTitleBar.css';
 export default function TitleBar({titre, nombre, onFiltreClick}){
     return (
         <div id='bar'>
             <div id='filtreBTN' onClick={onFiltreClick}>
-                <span>Filtres</span>
+                <span> <img src={filter} height="25px" /> Filtres</span>
             </div>
             <div id='titre'> {titre} </div>
             <div id='nombre'> {nombre} </div>
