ea47a8f05f5ef06c9f456aaadde3078bbddb63f0
Guymaster <67753626+Guymaster@users.noreply.github.com>
2022-05-07 16:50:13 +0000

    Update fff.js

--- fff.js
+++ fff.js
@@ -15,3 +15,28 @@ unction getRechargFromFiltres(dateDeb, dateFin, montantMin, montantMax, telephon
 
     url = encodeURI(req);
 }
+
+
+function getProprioFromFiltres(dateDeb, dateFin, nombreConduMin, nombreConduMax, commune, quartier){
+    let req = 'requetes/getProprioFromFiltres.php?dateDeb='+dateDeb+'&dateFin='+dateFin+'&nombreConduMin='+nombreConduMin+'&nombreConduMax='+nombreConduMax;
+    if(commune != ''){
+        req = req + '&communeCondu='+commune;
+    }
+    if(quartier != ''){
+        req = req + '&quartierCondu='+quartier;
+    }
+
+    let url = encodeURI(req);
+}
+
+function getConductFromFiltres(dateDeb, dateFin, commune, quartier){
+    let req = 'requetes/getConductFromFiltres.php?dateDeb='+dateDeb+'&dateFin='+dateFin;
+    if(commune != ''){
+        req = req + '&commune='+commune;
+    }
+    if(quartier != ''){
+        req = req + '&quartier='+quartier;
+    }
+
+    let url = encodeURI(req);
+}
